6c8dedaddd9ac4225d8ff4183e107b9de078eaf9
Edinei J. Bauer <galera.org@gmail.com>
2020-07-22 20:18:43 -0300

    mensagens with pendente

--- public/get/event/chatOnline.php
+++ public/get/event/chatOnline.php
@@ -1,13 +1,11 @@
 <?php
 
-use Conn\Update;
-
 /**
  * Update status online
  */
 if (!empty($_SESSION['userlogin']) && !empty($_SESSION['userlogin']['token']) && !empty($variaveis[0])) {
-    $up = new Update();
-    $up->exeUpdate("messages_user", ["ultima_vez_online" => date("Y-m-d H:i:s")], "WHERE ownerpub = :u AND usuario = :me AND bloqueado = 0", "me={$_SESSION['userlogin']['id']}&u={$variaveis[0]}");
+    $up = new \Conn\Update();
+    $up->exeUpdate("messages_user", ["ultima_vez_online" => date("Y-m-d H:i:s")], "WHERE ownerpub = {$variaveis[0]} && usuario = {$_SESSION['userlogin']['id']} && bloqueado = 0");
 
     $read = new \Conn\Read();
     $read->exeRead("messages_user", "WHERE usuario = :u AND bloqueado = 0", "u={$variaveis[0]}");
