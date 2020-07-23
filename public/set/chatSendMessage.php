ea593493214579c028ccb30db34452634bf6f870
Edinei J. Bauer <galera.org@gmail.com>
2020-07-23 14:16:00 -0300

    check if the user blocked me

--- public/set/chatSendMessage.php
+++ public/set/chatSendMessage.php
@@ -4,6 +4,7 @@ $user = filter_input(INPUT_POST, 'usuario', FILTER_VALIDATE_INT);
 $mensagem = trim(filter_input(INPUT_POST, 'mensagem', FILTER_DEFAULT));
 
 if(!empty($_SESSION['userlogin']) && !empty($_SESSION['userlogin']['token']) && !empty($user)) {
+    $read = new \Conn\Read();
     \Helpers\Helper::createFolderIfNoExist(PATH_HOME . "_cdn/chat");
     \Helpers\Helper::createFolderIfNoExist(PATH_HOME . "_cdn/chat/" . $user);
     \Helpers\Helper::createFolderIfNoExist(PATH_HOME . "_cdn/chat/" . $user . "/pending");
@@ -13,26 +14,31 @@ if(!empty($_SESSION['userlogin']) && !empty($_SESSION['userlogin']['token']) && 
      * Create a file that store the message to the user
      * this store say to user that have pending messages
      */
-    $f = fopen(PATH_HOME . "_cdn/chat/" . $user . "/pending/" . $_SESSION['userlogin']['id'] . "/" . strtotime('now') . "-" . rand(99999, 999999) . ".txt", "w+");
-    fwrite($f, json_encode(["mensagem" => $mensagem, "usuario" => $_SESSION['userlogin']['id'], "data" => strtotime('now')]));
-    fclose($f);
-
-    /**
-     * Check if the user accept Notification
-     */
-    $read = new \Conn\Read();
-    $read->exeRead("messages_user", "WHERE ownerpub = :user AND usuario = :me AND silenciado = 0", "user={$user}&me={$_SESSION['userlogin']['id']}", !0);
+    $read->exeRead("messages_user", "WHERE ownerpub = :user AND usuario = :me", "user={$user}&me={$_SESSION['userlogin']['id']}", !0);
     if($read->getResult()) {
+        $m = $read->getResult()[0];
+
+        if($m['bloqueado'] == 0) {
+            $f = fopen(PATH_HOME . "_cdn/chat/" . $user . "/pending/" . $_SESSION['userlogin']['id'] . "/" . strtotime('now') . "-" . rand(99999, 999999) . ".txt", "w+");
+            fwrite($f, json_encode(["mensagem" => $mensagem, "usuario" => $_SESSION['userlogin']['id'], "data" => strtotime('now')]));
+            fclose($f);
+        }
 
         /**
-         * Notify user from new message incoming from this user
+         * Check if the user accept Notification
          */
-        $note = new \Dashboard\Notification();
-        $note->setUsuarios($user);
-        $note->setTitulo($_SESSION['userlogin']['nome']);
-        $note->setDescricao($mensagem);
-        $note->setImagem(!empty($_SESSION['userlogin']['imagem']) ? $_SESSION['userlogin']['imagem'][0]['url'] : HOME . "assetsPublic/img/favicon.png");
-        $note->setUrl(HOME . "mensagem/" . $_SESSION['userlogin']['id']);
-        $note->enviar();
+        if($m['silenciado'] == 0) {
+
+            /**
+             * Notify user from new message incoming from this user
+             */
+            $note = new \Dashboard\Notification();
+            $note->setUsuarios($user);
+            $note->setTitulo($_SESSION['userlogin']['nome']);
+            $note->setDescricao($mensagem);
+            $note->setImagem(!empty($_SESSION['userlogin']['imagem']) ? $_SESSION['userlogin']['imagem'][0]['url'] : HOME . "assetsPublic/img/favicon.png");
+            $note->setUrl(HOME . "mensagem/" . $_SESSION['userlogin']['id']);
+            $note->enviar();
+        }
     }
 }
