c23f200f77808c597204938cd61d237737e2fbef
Edinei J. Bauer <galera.org@gmail.com>
2020-07-24 09:19:22 -0300

    update chat one time

--- public/assets/badgeMessageNotification.js
+++ public/assets/badgeMessageNotification.js
@@ -2,40 +2,45 @@
  * Verifica se tem notificações pendentes
  * @returns {Promise<void>}
  */
-var messageBadgeNote = null;
-function _messageBadgeShow(badge) {
-    let $navbar = $("#core-header-nav-bottom").find("a[href='mensagens']");
-    $navbar.find(".badge-notification").remove();
+if(typeof messageBadgeNote === "undefined") {
+    var messageBadgeNote = null;
 
-    /**
-     * Adiciona badge notification apenas no navbar mobile e se tiver a aba de notificações
-     */
-    if (badge !== "0")
-        $navbar.append("<span class='badge-notification'>" + badge + "</span>");
-}
+    function _messageBadgeShow(badge) {
+        let $navbar = $("#core-header-nav-bottom").find("a[href='mensagens']");
+        $navbar.find(".badge-notification").remove();
 
-async function messageBadgeNotification() {
-    if ($("#core-header-nav-bottom").find("a[href='mensagens']").length && USER.setor !== 0) {
-        if (typeof (EventSource) !== "undefined" && HOME !== "" && HOME === SERVER) {
-            if(messageBadgeNote)
-                messageBadgeNote.close();
+        /**
+         * Adiciona badge notification apenas no navbar mobile e se tiver a aba de notificações
+         */
+        if (badge !== "0")
+            $navbar.append("<span class='badge-notification'>" + badge + "</span>");
+    }
 
-            messageBadgeNote = new EventSource(SERVER + "get/event/sse/chatBadgeNotification/maestruToken/" + USER.token, {withCredentials: true});
+    async function messageBadgeNotification() {
+        if (!messageBadgeNote) {
+            if ($("#core-header-nav-bottom").find("a[href='mensagens']").length && USER.setor !== 0) {
+                if (typeof (EventSource) !== "undefined" && HOME !== "" && HOME === SERVER) {
+                    if (messageBadgeNote)
+                        messageBadgeNote.close();
 
-            messageBadgeNote.onmessage = function (event) {
-                _messageBadgeShow(event.data);
-            };
-        } else {
-            if(messageBadgeNote)
-                clearInterval(messageBadgeNote);
+                    messageBadgeNote = new EventSource(SERVER + "get/event/sse/chatBadgeNotification/maestruToken/" + USER.token, {withCredentials: true});
 
-            messageBadgeNote = setInterval(async function () {
-                _messageBadgeShow(await AJAX.get("event/chatBadgeNotification"));
-            }, 2000);
+                    messageBadgeNote.onmessage = function (event) {
+                        _messageBadgeShow(event.data);
+                    };
+                } else {
+                    if (messageBadgeNote)
+                        clearInterval(messageBadgeNote);
+
+                    messageBadgeNote = setInterval(async function () {
+                        _messageBadgeShow(await AJAX.get("event/chatBadgeNotification"));
+                    }, 2000);
+                }
+            }
         }
     }
-}
 
-$(function () {
-    messageBadgeNotification();
-})
+    $(function () {
+        messageBadgeNotification();
+    });
+}
