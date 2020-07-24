be291cf9077998e36dd98fcf1b43e3a24d107e59
Edinei J. Bauer <galera.org@gmail.com>
2020-07-24 09:42:52 -0300

    is writing check if is in 10s from the typing

--- public/get/event/chatUpdate.php
+++ public/get/event/chatUpdate.php
@@ -132,7 +132,7 @@ if (!empty($_SESSION['userlogin']) && !empty($_SESSION['userlogin']['token']) &&
      */
     if (file_exists(PATH_HOME . "_cdn/chat/{$_SESSION['userlogin']['id']}/{$user}.txt")) {
         $dia = file_get_contents(PATH_HOME . "_cdn/chat/{$_SESSION['userlogin']['id']}/{$user}.txt");
-        if ($dia < strtotime() - 10)
+        if ($dia < strtotime('now') - 10)
             $mensagens[] = ["mensagem" => "~^", "usuario" => $user, "data" => $dia];
 
         unlink(PATH_HOME . "_cdn/chat/{$_SESSION['userlogin']['id']}/{$user}.txt");
